d725ac8106610c071783465fd7e71052481cc915
Sebastian <s.albertogamarro5@gmail.com>
2022-04-20 14:51:03 -0600

    saved changes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const dbConn = require('../db/db')
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     
         dbConn.query('SELECT * FROM to_do ORDER BY id', (err,rows) =>{
             if(err){
@@ -14,5 +14,38 @@ router.get('/', function (req, res, next) {
         })
     
 })
+
+router.post('/add', (req,res) =>{
+    const data = req.body.chores
+    let errors = false
+    
+    if(data.length === 0) {
+        errors = true;
+
+        // set flash message
+        req.flash('error', "Please enter name and author");
+        // render to add.ejs with flash message
+        res.render('/', {
+            data:data
+        })
+    }
+
+    if(!errors){
+    dbConn.query('INSERT INTO to_do (chores) VALUES(?)', data, function(err, result) {
+        //if(err) throw err
+        if (err) {
+            req.flash('error', err)
+
+            // render to add.ejs
+                res.redirect('/')
+        } else {
+            req.flash('success', 'Book successfully added');
+            res.redirect('/');
+        }
+    })
+
+}
+})
+
 
 module.exports = router;
